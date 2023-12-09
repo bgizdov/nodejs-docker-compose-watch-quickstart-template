67cbf9673700c7e1776952000f4266053ce3f94c
Borislav Gizdov <borislav.gizdov@gmail.com>
2023-12-09 17:47:40 +0200

    Removed whitespace

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello superhero '     + sh.random())
+    res.send('Hello superhero ' + sh.random())
 });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.\nOpen http://localhost:3000/ in your browser.'));
